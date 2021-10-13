c507a2a46efdc00564bf42df79ac236297233803
Amanda L Martin <hythloda@gmail.com>
2021-10-13 17:30:32 -0400

    another function

--- RedpandaStats/kafka-produce.py
+++ RedpandaStats/kafka-produce.py
@@ -31,7 +31,7 @@ producer = Producer({
     'bootstrap.servers': 'localhost:9092',
 })
 
-def convertUnit(inputUnit):
+def convert_unit(inputUnit):
     if inputUnit ==  'GiB': return 1073741824
     if inputUnit ==  'MiB': return 1048576
     if inputUnit ==  'kiB': return 1024
@@ -39,6 +39,13 @@ def convertUnit(inputUnit):
     if inputUnit ==  'MB': return 1000000
     if inputUnit ==  'kB': return 1000
     else: return 1
+
+
+def get_raw(value_with_unit_str):
+  value_str = re.findall('\d*\.?\d+',value_with_unit_str)[0]
+  unit_str = value_with_unit_str[len(value_str):]
+  return int(float(value_str) * float(convert_unit(unit_str)))
+
 
 while True:
     data = subprocess.check_output("docker stats --no-stream", shell=True).decode('utf8')
@@ -53,13 +60,13 @@ while True:
             "container": args[0],
             "name": args[1],
             "cpuPercent": re.findall('\d*\.?\d+',args[2])[0],
-            "memoryUsage": int(float(re.findall('\d*\.?\d+',args[3])[0])*float(convertUnit(args[3][len(re.findall('\d*\.?\d+',args[3])[0]):]))),
-            "memoryLimit": int(float(re.findall('\d*\.?\d+',args[5])[0])*float(convertUnit(args[5][len(re.findall('\d*\.?\d+',args[5])[0]):]))),
+            "memoryUsage": get_raw(args[3]),
+            "memoryLimit": get_raw(args[5]),
             "memoryPercent": re.findall('\d*\.?\d+',args[6])[0],
-            "networkInput": int(float(re.findall('\d*\.?\d+',args[7])[0])*float(convertUnit(args[7][len(re.findall('\d*\.?\d+',args[7])[0]):]))),
-            "networkOutput": int(float(re.findall('\d*\.?\d+',args[9])[0])*float(convertUnit(args[9][len(re.findall('\d*\.?\d+',args[9])[0]):]))),
-            "blockInput": int(float(re.findall('\d*\.?\d+',args[10])[0])*float(convertUnit(args[10][len(re.findall('\d*\.?\d+',args[10])[0]):]))),
-            "blockOutput": int(float(re.findall('\d*\.?\d+',args[12])[0])*float(convertUnit(args[12][len(re.findall('\d*\.?\d+',args[12])[0]):]))),
+            "networkInput": get_raw(args[7]),
+            "networkOutput": get_raw(args[9]),
+            "blockInput": get_raw(args[10]),
+            "blockOutput": get_raw(args[12]),
             "pids": re.findall('\d*\.?\d+',args[13])[0]
             }
 
