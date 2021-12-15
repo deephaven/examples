5bd87bc2193c2a6af56db9808f444d0fff923099
mikebender <mikebender@deephaven.io>
2021-12-15 09:39:45 -0500

    Clean up App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,20 +11,6 @@ import { setWorkspace, store } from '@deephaven/redux';
 
 import './App.scss';
 
-// There is a proxy to redirect all traffic from our server to localhost:10000 defined in package.json by default.
-// Change the proxy there if you wish to connect to a different address.
-// https://create-react-app.dev/docs/proxying-api-requests-in-development/
-const websocketUrl = `${window.location}`;
-
-// For this example, we're using python.
-const consoleType = 'python';
-
-// Code to create an empty table
-const emptyTableCode = `
-from deephaven.TableTools import timeTable
-t = timeTable("00:00:02").update("A=i")
-`;
-
 // We need to define the workspace so the proper time zone is used in the grid
 store.dispatch(setWorkspace({
   data: {
@@ -46,17 +32,24 @@ function App() {
 
   const initApp = useCallback(async () => {
     try {
-      setMessage(`Starting connection to ${websocketUrl}...`);
-    
-      const connection = new dh.IdeConnection(websocketUrl);
+      // Connect to the Web API server. By default we just use the same location this server is hosted on.
+      // There is a proxy to redirect all traffic from our server to localhost:10000 defined in package.json by default.
+      // Change the proxy there if you wish to connect to a different address.
+      // https://create-react-app.dev/docs/proxying-api-requests-in-development/
+      setMessage(`Starting connection...`);
+      const connection = new dh.IdeConnection(`${window.location}`);
   
-      setMessage(`Starting session with type ${consoleType}...`);
-    
-      const session = await connection.startSession(consoleType);
+      // Start a code session. For this example, we use python
+      setMessage(`Starting session...`);
+      const session = await connection.startSession('python');
   
+      // Run code to create a ticking table
+      // This table will tick once every second, with a column A indicating the row number
       setMessage(`Creating table...`);
-  
-      const result = await session.runCode(emptyTableCode);
+      const result = await session.runCode(`
+      from deephaven.TableTools import timeTable
+      t = timeTable("00:00:01").update("A=i")
+      `);
   
       const createdObject = result.changes.created[0];
   
