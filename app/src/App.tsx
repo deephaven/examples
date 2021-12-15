c362e451c7d4dfd905074927cf205f2874f162e6
mikebender <mikebender@deephaven.io>
2021-12-15 10:30:12 -0500

    Open an existing table with the `tableName` param set

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,6 +12,12 @@ import dh from "@deephaven/jsapi-shim";
 
 import "./App.scss";
 
+/**
+ * A simple React App that displays a Deephaven table in an IrisGrid using the @deephaven/iris-grid package.
+ * If the query param `tableName` is provided, it will attempt to open and display that table, expecting it to be present on the server.
+ * E.g. http://localhost:3000?tableName=myTable will attempt to open a table `myTable`
+ * If no query param is provided, it will attempt to open a new session and create a basic time table and display that.
+ */
 function App() {
   const [model, setModel] = useState<IrisGridModel>();
   const [message, setMessage] = useState("");
@@ -22,32 +28,58 @@ function App() {
       // There is a proxy to redirect all traffic from our server to localhost:10000 defined in package.json by default.
       // Change the proxy there if you wish to connect to a different address.
       // https://create-react-app.dev/docs/proxying-api-requests-in-development/
+
+      const baseUrl = new URL("", `${window.location}`);
+
+      const websocketUrl = `${baseUrl.protocol}//${baseUrl.host}`;
+
       setMessage(`Starting connection...`);
-      const connection = new dh.IdeConnection(`${window.location}`);
+      const connection = new dh.IdeConnection(websocketUrl);
+
+      const types = await connection.getConsoleTypes();
+
+      console.log("Available types:", types);
 
       // Start a code session. For this example, we use python
       setMessage(`Starting session...`);
       const session = await connection.startSession("python");
 
-      // Run code to create a ticking table
-      // This table will tick once every second, with a column A indicating the row number
-      setMessage(`Creating table...`);
-      await session.runCode("from deephaven.TableTools import timeTable");
-      const result = await session.runCode(
-        't = timeTable("00:00:01").update("A=i")'
-      );
+      const searchParams = new URLSearchParams(window.location.search);
+      const tableName = searchParams.get("tableName");
 
-      const createdObject = result.changes.created[0];
+      if (tableName) {
+        // We should try and open the existing table
+        setMessage(`Retrieving table ${tableName}...`);
 
-      setMessage(`Retrieving table ${createdObject.name}...`);
+        const definition = { name: tableName, type: dh.VariableType.TABLE };
+        const table = await session.getObject(definition);
 
-      const table = await session.getObject(createdObject);
+        setMessage(`Creating model...`);
 
-      setMessage(`Creating model...`);
+        const newModel = await IrisGridModelFactory.makeModel(table);
 
-      const newModel = await IrisGridModelFactory.makeModel(table);
+        setModel(newModel);
+      } else {
+        // Run code to create a ticking table
+        // This table will tick once every second, with a column A indicating the row number
+        setMessage(`Creating table...`);
+        await session.runCode("from deephaven.TableTools import timeTable");
+        const result = await session.runCode(
+          't = timeTable("00:00:01").update("A=i")'
+        );
 
-      setModel(newModel);
+        const createdObject = result.changes.created[0];
+
+        setMessage(`Retrieving table ${createdObject.name}...`);
+
+        const table = await session.getObject(createdObject);
+
+        setMessage(`Creating model...`);
+
+        const newModel = await IrisGridModelFactory.makeModel(table);
+
+        setModel(newModel);
+      }
     } catch (err) {
       setMessage(`Error: ${err}`);
     }
