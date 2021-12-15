f0ad94c4b4e5dee912178214d0768fa5b184a5d0
mikebender <mikebender@deephaven.io>
2021-12-15 09:55:08 -0500

    Clean up example to use latest versions of packages
    No more redux!

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,20 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
+import React, { useCallback, useEffect, useState } from "react";
 
-// iris-grid is used to display Deephaven tables. 
-import { Formatter, IrisGrid, IrisGridModel, IrisGridModelFactory } from '@deephaven/iris-grid';
-import { DateTimeColumnFormatter } from '@deephaven/iris-grid/dist/formatters';
+// iris-grid is used to display Deephaven tables.
+import {
+  IrisGrid,
+  IrisGridModel,
+  IrisGridModelFactory,
+} from "@deephaven/iris-grid";
 
 // Import the shim to use the JS API
-import dh from '@deephaven/jsapi-shim';
-import { setWorkspace, store } from '@deephaven/redux';
+import dh from "@deephaven/jsapi-shim";
 
-import './App.scss';
-
-// We need to define the workspace so the proper time zone is used in the grid
-store.dispatch(setWorkspace({
-  data: {
-    settings: {
-      defaultDateTimeFormat:
-        DateTimeColumnFormatter.DEFAULT_DATETIME_FORMAT_STRING,
-      formatter: Formatter.getDefaultFormattingRules(),
-      timeZone: DateTimeColumnFormatter.DEFAULT_TIME_ZONE_ID,
-      showTimeZone: false,
-      showTSeparator: true,
-      disableMoveConfirmation: false,
-    },
-  }
-}));
+import "./App.scss";
 
 function App() {
-  const [ model, setModel ] = useState<IrisGridModel>();
-  const [ message, setMessage ] = useState('');
+  const [model, setModel] = useState<IrisGridModel>();
+  const [message, setMessage] = useState("");
 
   const initApp = useCallback(async () => {
     try {
@@ -38,29 +24,29 @@ function App() {
       // https://create-react-app.dev/docs/proxying-api-requests-in-development/
       setMessage(`Starting connection...`);
       const connection = new dh.IdeConnection(`${window.location}`);
-  
+
       // Start a code session. For this example, we use python
       setMessage(`Starting session...`);
-      const session = await connection.startSession('python');
-  
+      const session = await connection.startSession("python");
+
       // Run code to create a ticking table
       // This table will tick once every second, with a column A indicating the row number
       setMessage(`Creating table...`);
-      const result = await session.runCode(`
-      from deephaven.TableTools import timeTable
-      t = timeTable("00:00:01").update("A=i")
-      `);
-  
+      await session.runCode("from deephaven.TableTools import timeTable");
+      const result = await session.runCode(
+        't = timeTable("00:00:01").update("A=i")'
+      );
+
       const createdObject = result.changes.created[0];
-  
+
       setMessage(`Retrieving table ${createdObject.name}...`);
-  
+
       const table = await session.getObject(createdObject);
-  
+
       setMessage(`Creating model...`);
-  
+
       const newModel = await IrisGridModelFactory.makeModel(table);
-  
+
       setModel(newModel);
     } catch (err) {
       setMessage(`Error: ${err}`);
@@ -69,16 +55,14 @@ function App() {
 
   useEffect(() => {
     initApp();
-  }, [ initApp]);
+  }, [initApp]);
 
   const isLoaded = model != null;
 
   return (
     <div className="App">
-      <Provider store={store}>
-        { !isLoaded && <div className="message">{message}</div>}
-        { isLoaded && <IrisGrid model={model}/>}
-      </Provider>
+      {!isLoaded && <div className="message">{message}</div>}
+      {isLoaded && <IrisGrid model={model} />}
     </div>
   );
 }
